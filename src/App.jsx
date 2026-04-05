28ef841bbb803d6c714ec5c2ab2b7cb54f7bdf16
Gabriel Pietro <gabriel.pietro@sptech.com>
2026-04-04 22:34:30 -0300

    feat: add tela de dashboard integrada com chart.js (mockado)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Header from './components/utils/Header'
+import { TelaDashboard } from './components/pages/TelaDashboard'
 
 function App() {
   return (
     <>
-      <Header />
+      <TelaDashboard></TelaDashboard>
     </>
   )
 }
