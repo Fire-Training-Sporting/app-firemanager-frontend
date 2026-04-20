f9c6f3da1d82c729478cf9ce80b48698f928cae3
Gabriel Pietro <gabriel.pietro@sptech.com>
2026-04-20 14:57:57 -0300

    feat: adicionei a lógica para startar pela tela de login e atraves de useState, if e "prop" mudar a página atual

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
-import TelaAlunos from './components/pages/TelaAlunos'
-import TelaFuncionarios from './components/pages/TelaFuncionarios'
+import { useState } from 'react';
+import './App.css';
+import { TelaLogin } from './components/pages/TelaLogin';
+import { TelaDashboard } from './components/pages/TelaDashboard';
 
 function App() {
+  const [telaAtiva, setTelaAtiva] = useState("login");
+
+  //LOGICA DE MUDAR DE TELA ATRAVES DO ESTADO
   return (
-    <>
-      {/* <TelaAlunos /> */}
-      {/* <TelaFuncionarios /> */}
-    </>
-  )
+    <div className="App">
+      {telaAtiva === "login" && (
+        <TelaLogin onLoginSucesso={() => setTelaAtiva("dashboard")} />
+      )}
+
+      {telaAtiva === "dashboard" && (
+        <TelaDashboard onLogout={() => setTelaAtiva("login")} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
