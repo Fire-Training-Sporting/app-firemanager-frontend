722f8f91eb7b0ff18cdbf2008065f270bfd466f8
thalitalourenco <thalita.lourenco@sptech.school>
2026-04-27 17:15:28 -0300

    feat: adicionando funcionalidade do google maps

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       {/* <TelaAlunos /> */}
       {/* <TelaFuncionarios /> */}
-      {/* <TelaAgendamentos /> */}
+      <TelaAgendamentos />
       {/* <TelaCondominios /> */}
       {/* <TelaLogin /> */}
     </>
--- src/components/utils/Agendamentos/AgendamentosTable.jsx
+++ src/components/utils/Agendamentos/AgendamentosTable.jsx
@@ -1,22 +1,31 @@
-import { AgendamentosRow } from './AgendamentosRow';
-import AgendamentosTh from './AgendamentosTh';
+import { useEffect, useState } from "react";
+import { AgendamentosRow } from "./AgendamentosRow";
+import AgendamentosTh from "./AgendamentosTh";
+import api from "../../../provider/api";
 
-const agendamentos = [
-  { id: 1, data: "16/02/26", horario: "10h - 11h", quadra: "Quadra A", prof: "Bruno", reba: "Davi", aux: "Kalleb", status: "Confirmada" },
-  { id: 2, data: "17/02/26", horario: "13h - 14h", quadra: "Quadra B", prof: "Paulo", reba: "Enzo", aux: "Lucas", status: "Pendente" },
-  // ... demais registros
-];
 
 export function AgendamentosTable() {
+  const [agendamentos, setAgendamentos] = useState([]);
+
+  useEffect(() => {
+    api.get("/agendamentos")
+      .then(response => {
+        console.log(response.data);
+        setAgendamentos(response.data);
+      })
+      .catch(err => console.error("Erro ao buscar agendamentos:", err));
+  }, []);
+
   return (
     <div className="w-full overflow-x-auto">
-      <div className="max-h-115 overflow-y-auto">
-        <table className="w-full border-separate border-spacing-0 rounded-lg overflow-hidden">
-          <thead className="sticky top-0 z-10">
+      <div className="min-h-[200px] max-h-[calc(100vh-300px)] overflow-y-auto">
+        <table className="w-full border-separate border-spacing-0 rounded-lg">
+          <thead className="sticky top-0 z-10 bg-white">
             <tr className="border-b-2 border-gray-200">
+              <AgendamentosTh>Aluno</AgendamentosTh>
               <AgendamentosTh>Data</AgendamentosTh>
               <AgendamentosTh>Horário</AgendamentosTh>
-              <AgendamentosTh>Quadra</AgendamentosTh>
+              <AgendamentosTh>Condomínio</AgendamentosTh>
               <AgendamentosTh>Prof</AgendamentosTh>
               <AgendamentosTh>Reba</AgendamentosTh>
               <AgendamentosTh>Aux</AgendamentosTh>
