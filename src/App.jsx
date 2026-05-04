3f4140cd79f233126230dcb097444992395f2348
Vitor-Gomesz <vitor.golieveira@sptech.school>
2026-05-04 01:23:04 -0300

    Fix: Correção de arquivos, voltando a logica anterior

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       {/* <TelaAlunos /> */}
-      {/* <TelaFuncionarios /> */}
+      <TelaFuncionarios /> 
       {/* <TelaAgendamentos /> */}
       {/* <TelaCondominios /> */}
       {/* <TelaLogin /> */}
