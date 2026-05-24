c64954eefe00182802d1181699511d0eda0c5a3d
Gabriel <gabriel.sramos@sptech.school>
2026-05-24 20:33:25 -0300

    add: adicionando integração com backend para criar e excluir um aluno

--- src/components/utils/Alunos/AlunosRow.jsx
+++ src/components/utils/Alunos/AlunosRow.jsx
@@ -1,35 +1,4 @@
-const colors = {
-  bg: {
-    gray: "bg-[#4F4F4F]",
-    darkGray: "bg-[#363636]",
-    orange: "bg-[#F99A4D]",
-    darkOrange: "bg-[#F8821E]",
-    white: "bg-[#F3F4F8]",
-    green: "bg-[#17A34A]",
-    red: "bg-[#DC2625]",
-    blue: "bg-[#2563EA]",
-    yellow: "bg-[#E9B308]",
-  },
-  text: {
-    red: "text-[#DC2625]",
-    blue: "text-[#2563EA]",
-    green: "text-[#17A34A]",
-    gray: "text-[#4F4F4F]",
-  },
-  hoverBg: {
-    red: "hover:bg-[#B91C1C]",      
-    blue: "hover:bg-[#1E40AF]",     
-    green: "hover:bg-[#166534]",    
-    orange: "hover:bg-[#EA580C]",   
-    gray: "hover:bg-[#2E2E2E]",     
-    yellow: "hover:bg-[#CA8A04]",   
-  },
-  hoverText: {
-    red: "hover:text-[#B91C1C]",
-  }
-};
-
-export function AlunosRow({ id, nome, email, telefone, endereco }) {
+export function AlunosRow({ id, nome, email, telefone, endereco, onDelete = () => {} }) {
   return (
     <tr className="border-b border-gray-200 hover:bg-[#F3F4F8] transition-colors duration-150">
       <td className="px-4 py-3 text-gray-800 font-normal text-sm align-middle w-12">{id}</td>
@@ -42,7 +11,7 @@ export function AlunosRow({ id, nome, email, telefone, endereco }) {
           <button className="px-4 py-2 bg-[#2563EA] text-white text-xs font-medium rounded-md hover:bg-[#1E40AF] shadow-sm hover:shadow-md transition-all duration-150 cursor-pointer">
             Editar
           </button>
-          <button className="px-4 py-2 bg-[#DC2625] text-white text-xs font-medium rounded-md hover:bg-[#B91C1C] shadow-sm hover:shadow-md transition-all duration-150 cursor-pointer">
+          <button onClick={onDelete} className="px-4 py-2 bg-[#DC2625] text-white text-xs font-medium rounded-md hover:bg-[#B91C1C] shadow-sm hover:shadow-md transition-all duration-150 cursor-pointer">
             Excluir
           </button>
         </div>
