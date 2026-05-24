5775964ec983b8cc14fd8c51cd29cbe21617fd9a
Gabriel <gabriel.sramos@sptech.school>
2026-05-24 20:38:01 -0300

    add: adicionando DElETE para funcionários e condomínios

--- src/components/utils/Funcionarios/TabelaFuncionarios.jsx
+++ src/components/utils/Funcionarios/TabelaFuncionarios.jsx
@@ -34,7 +34,7 @@ export default function TabelaFuncionarios({ funcionarios = [], onEdit = () => {
       render: (row) => (
         <div className="flex justify-center gap-2">
           <button onClick={() => onEdit(row)} className="px-4 py-2 bg-[#2563EA] text-white text-xs font-medium rounded-md hover:bg-[#1E40AF] shadow-sm hover:shadow-md transition-all duration-150 cursor-pointer">Editar</button>
-          <button onClick={() => onDelete(row.id)} className="px-4 py-2 bg-[#DC2625] text-white text-xs font-medium rounded-md hover:bg-[#B91C1C] shadow-sm hover:shadow-md transition-all duration-150 cursor-pointer">Excluir</button>
+          <button onClick={() => onDelete(row)} className="px-4 py-2 bg-[#DC2625] text-white text-xs font-medium rounded-md hover:bg-[#B91C1C] shadow-sm hover:shadow-md transition-all duration-150 cursor-pointer">Excluir</button>
         </div>
       ),
     },
