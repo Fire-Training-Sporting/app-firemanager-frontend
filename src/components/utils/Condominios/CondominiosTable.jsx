b7b88a54947888194c2bd6a39b179ac2a3e2a26f
Gabriel <gabriel.sramos@sptech.school>
2026-05-30 18:36:34 -0300

    fix: escondendo saldo dos alunos dos professores + ajustando tabela de condominios de rua para logradouro

--- src/components/utils/Condominios/CondominiosTable.jsx
+++ src/components/utils/Condominios/CondominiosTable.jsx
@@ -73,7 +73,7 @@ export function CondominiosTable({
               </CondominiosTh>
 
               <CondominiosTh>
-                Rua
+                Logradouro
               </CondominiosTh>
 
               <CondominiosTh>
